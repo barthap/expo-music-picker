b985caa3753199bdacdd4f51d38f34ae99bc56dd
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-07-01 12:29:29 +0200

    Example code

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
 
-import * as ExpoMusicPicker from 'expo-music-picker';
+import * as MusicPicker from "../src/ExpoMusicPicker";
+import { useState } from "react";
 
 export default function App() {
+  const [items, setItems] = useState<MusicPicker.MusicItem[]>([]);
+
+  const pickMediaAsync = async () => {
+    try {
+      const result = await MusicPicker.openMusicLibraryAsync({
+        allowMultipleSelection: false,
+      });
+
+      if (!result.cancelled) {
+        setItems(result.items);
+      }
+    } catch (e) {
+      console.warn("Exception occurred when picking music:", e);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>{ExpoMusicPicker.hello()}</Text>
+      <TouchableHighlight onPress={pickMediaAsync}>
+        <Text>Open music library</Text>
+      </TouchableHighlight>
+      <Text>{JSON.stringify(items, null, 2)}</Text>
     </View>
   );
 }
@@ -13,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
