a879251565ee71bd2da66fdb1d66fe9d380862ad
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-07-14 10:10:10 +0200

    Improve demo app

--- example/App.tsx
+++ example/App.tsx
@@ -1,7 +1,42 @@
-import { Image, StyleSheet, Text, View, Button } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  FlatList,
+  ScrollView,
+} from "react-native";
 
 import * as MusicPicker from "expo-music-picker";
 import { useState } from "react";
+import { MusicItem } from "expo-music-picker";
+import { Platform } from "expo-modules-core";
+
+function ItemElement({ item, index }: { item: MusicItem; index: number }) {
+  const coverImgSrc = item.artworkImage
+    ? { uri: `data:image/jpeg;base64,${item.artworkImage.base64Data}` }
+    : require("./assets/album-placeholder.png");
+  return (
+    <View style={styles.listItem} key={item.id ?? index}>
+      <Image
+        style={styles.artworkImage}
+        resizeMode="contain"
+        source={coverImgSrc}
+      />
+
+      <Text numberOfLines={1} style={styles.title}>
+        {item.title || "Untitled"}
+      </Text>
+      <Text numberOfLines={1} style={styles.artistName}>
+        {item.artist || "Unknown artist"}
+      </Text>
+      <Text numberOfLines={1} style={styles.albumName}>
+        {item.album || "Unknown album"}
+      </Text>
+    </View>
+  );
+}
 
 export default function App() {
   const [items, setItems] = useState<MusicPicker.MusicItem[]>([]);
@@ -11,7 +46,7 @@ export default function App() {
       const permissionResult = await MusicPicker.requestPermissionsAsync();
 
       if (!permissionResult.granted) {
-        console.warn("No permission");
+        console.warn("No permission. Open settings and grant please");
         return;
       }
 
@@ -28,25 +63,41 @@ export default function App() {
     }
   };
 
-  const finalItmes = items.map((it) => {
+  const strippedItems = items.map((it) => {
     const { artworkImage, ...rest } = it;
-    console.log(artworkImage?.width, artworkImage?.height);
-    return artworkImage?.base64Data ? rest : it;
+    return {
+      ...rest,
+      artworkImage: artworkImage
+        ? {
+            ...artworkImage,
+            base64Data: artworkImage.base64Data.substring(0, 20).concat("..."),
+          }
+        : null,
+    };
   });
 
   return (
     <View style={styles.container}>
       <Button title="Open music library" onPress={pickMediaAsync} />
-      <Text>{JSON.stringify(finalItmes, null, 2)}</Text>
-      {items[0]?.artworkImage?.base64Data && (
-        <Image
-          source={{
-            uri: `data:image/jpeg;base64,${items[0].artworkImage.base64Data}`,
-          }}
-          style={{ width: 200, height: 200 }}
-          resizeMode="contain"
-        />
-      )}
+      <FlatList
+        style={styles.list}
+        horizontal
+        data={items}
+        renderItem={(props) => <ItemElement {...props} />}
+        ListEmptyComponent={
+          <View style={styles.container}>
+            <Text>No song selected.</Text>
+            <Text>Open the picker and select one. Or more.</Text>
+          </View>
+        }
+        contentContainerStyle={styles.listContainer}
+      />
+      <Text style={styles.title}>Raw result:</Text>
+      <ScrollView style={styles.resultBox}>
+        <Text style={styles.monoText}>
+          {JSON.stringify(strippedItems, null, 2)}
+        </Text>
+      </ScrollView>
     </View>
   );
 }
@@ -57,5 +108,45 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    width: "100%",
+  },
+  list: {
+    maxHeight: 250,
+  },
+  listContainer: {
+    backgroundColor: "#fff",
+  },
+  listItem: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10,
+    width: 180,
+    height: 220,
+    backgroundColor: "#f0f0f5",
+    margin: 10,
+    borderRadius: 5,
+  },
+  artworkImage: {
+    width: 150,
+    height: 150,
+    marginBottom: 5,
+    borderRadius: 5,
+  },
+  title: {
+    fontWeight: "bold",
+  },
+  artistName: {},
+  albumName: {
+    fontStyle: "italic",
+  },
+  resultBox: {
+    width: "90%",
+    padding: 10,
+    maxHeight: 250,
+    height: 250,
+    backgroundColor: "#eee",
+  },
+  monoText: {
+    fontFamily: Platform.OS === "ios" ? "Courier" : "monospace",
   },
 });
